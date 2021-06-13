0ab27ebde1627e358d3d4cee85cb41b5047309ca
gatsby <dhunganasaroj3@gmail.com>
2021-06-13 14:48:24 +0545

    some file changes

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,48 +5,45 @@ import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
 import Dotenv from "dotenv-webpack";
 
-const webpackConfig = (env): Configuration => {
-    console.log({env}, "<<<<<<<<<<<<<,");
-    return {
-        entry: "./src/index.tsx",
-        ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
-        resolve: {
-            extensions: [".ts", ".tsx", ".js"],
-            plugins: [new TsconfigPathsPlugin()]
-        },
-        output: {
-            path: path.join(__dirname, "/build"),
-            filename: "build.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: "ts-loader",
-                    options: {
-                        transpileOnly: true
-                    },
-                    exclude: /dist/
-                }
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "./public/index.html"
-            }),
-            new Dotenv(),
-            new webpack.DefinePlugin({
-                "process.env.PRODUCTION": env.production || !env.development,
-                "process.env.NAME": JSON.stringify(require("./package.json").name),
-                "process.env.VERSION": JSON.stringify(require("./package.json").version)
-            }),
-            new ForkTsCheckerWebpackPlugin({
-                eslint: {
-                    files: "./src/**/*.{ts,tsx,js,jsx}" // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
-                }
-            })
+const webpackConfig = (env): Configuration => ({
+    entry: "./src/index.tsx",
+    ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        plugins: [new TsconfigPathsPlugin()]
+    },
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true
+                },
+                exclude: /dist/
+            }
         ]
-    };
-};
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new Dotenv(),
+        new webpack.DefinePlugin({
+            "process.env.PRODUCTION": env.production || !env.development,
+            "process.env.NAME": JSON.stringify(require("./package.json").name),
+            "process.env.VERSION": JSON.stringify(require("./package.json").version)
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: "./src/**/*.{ts,tsx,js,jsx}" // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+            }
+        })
+    ]
+});
 
 export default webpackConfig;
